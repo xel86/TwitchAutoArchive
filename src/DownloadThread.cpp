8b90ccd31924500b7c9e517649d8fe29dbb9ad83
xel86 <xel@xel.sh>
2022-07-23 15:37:22 -0400

    Less logs + mkv

--- src/DownloadThread.cpp
+++ src/DownloadThread.cpp
@@ -12,7 +12,7 @@ bool DownloadThread::streamlinkDownloadFunc(std::string user_login,
                                             std::filesystem::path dir) {
     std::string url = "https://twitch.tv/" + user_login;
     std::time_t result = std::time(nullptr);
-    dir /= (user_login + "-" + std::to_string(result) + ".mp4");
+    dir /= (user_login + "-" + std::to_string(result) + ".mkv");
 
     LOG.write(LogLevel::Always, "Opening stream " + url + " to download.");
 
